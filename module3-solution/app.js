cd6d3350d6f599cd936aaf0cdf2f732eb0d4eed6
lavanya <contactlavanya@gmail.com>
2017-11-26 15:43:51 -0800

    changed files

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -10,7 +10,7 @@ angular.module('NarrowItDownApp', [])
 
 function NarrowItDownAppDirective() {
   var ddo = {
-    templateUrl: 'listitem.html',
+    templateUrl:'listItem.html',
     scope: {
       items: '<',
       myTitle: '@title',
